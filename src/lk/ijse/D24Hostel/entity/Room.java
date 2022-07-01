eb2d020c417e4a915e50b128df745845bf885669
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-07-01 19:56:44 +0530

    Created Room Entity

--- src/lk/ijse/D24Hostel/entity/Room.java
+++ src/lk/ijse/D24Hostel/entity/Room.java
@@ -1,4 +1,32 @@
 package lk.ijse.D24Hostel.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Room {
+    @Id
+    private String roomTypeID;
+    @Column(nullable = false)
+    private String roomType;
+    @Column(nullable = false)
+    private String keyMoney;
+    @Column(nullable = false)
+    private int qty;
+    @Column(nullable = false)
+    private String status;
+
+    @OneToMany(mappedBy = "room", cascade = CascadeType.ALL)
+    private List<Reservation> reservationList = new ArrayList<>();
+
 }
+
--- src/lk/ijse/D24Hostel/entity/Student.java
+++ src/lk/ijse/D24Hostel/entity/Student.java
@@ -1,4 +1,42 @@
 package lk.ijse.D24Hostel.entity;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Student {
+    @Id
+    private String sId;
+    @Column(nullable = false)
+    private String name;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String contactNumber;
+    @Column(columnDefinition = "DATE", nullable = false)
+    private LocalDate dob;
+    @Column(nullable = false)
+    private String gender;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Reservation> reservationList = new ArrayList<>();
+
+    public Student(String id, String name, String address, String contactNumber, LocalDate dob, String gender) {
+        this.sId = id;
+        this.name = name;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.dob = dob;
+        this.gender = gender;
+    }
 }
