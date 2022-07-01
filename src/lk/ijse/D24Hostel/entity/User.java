893ae98dd6033429a437cf10669a1baa82ed3cbf
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-07-01 20:00:34 +0530

    Created User Entity

--- src/lk/ijse/D24Hostel/entity/User.java
+++ src/lk/ijse/D24Hostel/entity/User.java
@@ -1,4 +1,20 @@
 package lk.ijse.D24Hostel.entity;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    private String user_id;
+    @Column(nullable = false)
+    private String user_name;
+    @Column(nullable = false)
+    private String password;
 }
