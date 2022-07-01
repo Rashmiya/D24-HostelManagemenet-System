ad7d5f66ba93dbd81a02dbb97523199a2a955764
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-07-01 20:58:33 +0530

    Dashboard Form UI and Navigation Part

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -14,7 +14,7 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        URL resource = getClass().getResource("lk/ijse/D24Hostel/view/assests/DashboardForm.fxml");
+        URL resource = getClass().getResource("lk/ijse/D24Hostel/view/DashboardForm.fxml");
         Parent load = FXMLLoader.load(resource);
         Scene scene = new Scene(load);
         primaryStage.setTitle("D24 Hostel Management System");
---
+++ src/lk/ijse/D24Hostel/controller/DashboardFormController.java
@@ -0,0 +1,71 @@
+package lk.ijse.D24Hostel.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashboardFormController {
+    public AnchorPane DashboardForm;
+
+    public void checkAvailableRoomsOnaction(ActionEvent actionEvent) {
+    }
+
+    public void manageStudentbtnOnAction(ActionEvent actionEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/StudentForm.fxml"));
+        Stage stage = (Stage) DashboardForm.getScene().getWindow();
+        stage.setScene(new Scene(load));
+        stage.show();
+    }
+
+    public void manageStudents(MouseEvent mouseEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/StudentForm.fxml"));
+        Stage stage = (Stage) DashboardForm.getScene().getWindow();
+        stage.setScene(new Scene(load));
+        stage.show();
+    }
+
+    public void LogOutbtnOnAction(ActionEvent actionEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"));
+        Stage stage = (Stage) DashboardForm.getScene().getWindow();
+        stage.setScene(new Scene(load));
+        stage.show();
+    }
+
+    public void manageRoomsbtnOnAction(ActionEvent actionEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/RoomForm.fxml"));
+        Stage stage = (Stage) DashboardForm.getScene().getWindow();
+        stage.setScene(new Scene(load));
+        stage.show();
+    }
+
+    public void manageRooms(MouseEvent mouseEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/RoomForm.fxml"));
+        Stage stage = (Stage) DashboardForm.getScene().getWindow();
+        stage.setScene(new Scene(load));
+        stage.show();
+    }
+
+    public void RoomReservationbtnOnAction(ActionEvent actionEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/ReservationForm.fxml"));
+        Stage stage = (Stage) DashboardForm.getScene().getWindow();
+        stage.setScene(new Scene(load));
+        stage.show();
+    }
+
+    public void RoomReservation(MouseEvent mouseEvent) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource("../view/ReservationForm.fxml"));
+        Stage stage = (Stage) DashboardForm.getScene().getWindow();
+        stage.setScene(new Scene(load));
+        stage.show();
+    }
+
+    public void checkAvailableRooms(MouseEvent mouseEvent) {
+
+    }
+}
